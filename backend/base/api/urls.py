08bb40d11ef6b684576c1cd7d8735bd462c63429
Deaath65 <deaath65@gmail.com>
2023-08-11 09:41:05 +0545

    api complete

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -7,10 +7,10 @@ from rest_framework_simplejwt.views import (
 )
 
 
-
 urlpatterns = [
     path('', views.getRoutes),
-    
+    path('notes/', views.getNotes),
+
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
