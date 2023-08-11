e76b0abcab5820789eba8689da8cfaf2df6c749b
Deaath65 <deaath65@gmail.com>
2023-08-11 09:24:20 +0545

    app,home,login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PrivateRoute from './utils/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
+
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <p> This is our react website </p>
+      <Router>
+        <AuthProvider>
+          <Header/>
+          <PrivateRoute component={HomePage} path="/" exact/>
+          <Route component={LoginPage} path="/login"/>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
